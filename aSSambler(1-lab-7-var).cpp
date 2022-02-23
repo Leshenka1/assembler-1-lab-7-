5084d03d3ed53bdaa8a6a3a21454be84979187e4
Leshenka1 <zubelaleksey@gmail.com>
2022-02-23 23:26:02 +0300

    add 1 lab 7 var

---
+++ aSSambler(1-lab-7-var).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void sum(int _a)
+{
+	int d = 10;
+	int result = 0;
+	__asm
+	{
+		mov eax, _a
+
+	begin_while:
+		cdq
+			div d
+			add result, edx
+			cmp eax, 0
+			jne begin_while
+
+		
+	}
+	std::cout << result;
+}
+
+int main()
+{
+	int a;
+
+	cin >> a;
+	sum(a);
+	return(0);
+
+}
